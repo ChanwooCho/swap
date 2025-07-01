66b2975e705e9f40446cd8adbda4a93be7f2b7b7
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-07-01 14:25:59 +0900

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -6,7 +6,7 @@
 int main() {
     const std::string filename = "/data/data/com.termux/files/home/large_file.bin";
     const std::size_t fileSize = 20 * 1024 * 1024; // 20MB
-    const std::size_t bufferSize = 1024 * 1024;    // 1MB
+    const std::size_t bufferSize = 512 * 1024;    // 512KB
 
     char buffer[bufferSize];
     std::fill(buffer, buffer + bufferSize, 'A');
